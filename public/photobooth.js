8387d21a9d2de8cdafb36ea628afb383771feed2
ckloi <ckloi@ucdavis.edu>
2017-05-20 16:16:56 -0700

    Update photobooth.js

--- public/photobooth.js
+++ public/photobooth.js
@@ -1,6 +1,5 @@
-function getMainPage() {
-  var oReq = new XMLHttpRequest();
-  var url = "http://138.68.25.50:8066/photoboothMain.html";
-  window.location.href = url;
+portNum = 8066;
 
+function getMainPage() {
+  window.location.href = "http://138.68.25.50:" + portNum + "/photoboothMain.html";
 }
